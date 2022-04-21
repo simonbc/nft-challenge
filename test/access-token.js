1514cc65680d88d40b33d04be46c357618274885
Simon B. Carstensen <simonbc@gmail.com>
2022-04-21 07:03:59 +0200

    Update contract tests

--- test/access-token.js
+++ test/access-token.js
@@ -8,30 +8,27 @@ describe("Access token", function () {
     [owner, account1, account2] = await ethers.getSigners();
 
     const AccessToken = await ethers.getContractFactory("AccessToken");
-    accessToken = await AccessToken.deploy("https://ipfs.io/ipfs/123");
+    accessToken = await AccessToken.deploy("https://ipfs.io/ipfs/");
 
     await accessToken.deployed();
   });
 
   it("Fetch tokens", async function () {
-    await accessToken.mint("foo", 10);
+    await accessToken.mint("foo", 10, 1, "123");
     expect((await accessToken.getTokens()).length).to.equal(1);
 
-    await accessToken.mint("bar", 10);
+    await accessToken.mint("bar", 10, 1, "123");
     expect((await accessToken.getTokens()).length).to.equal(2);
   });
 
   it("Authentication", async function () {
-    await accessToken.mint("foo", 10);
+    await accessToken.mint("foo", 10, 1, "123");
 
-    await accessToken.safeTransferFrom(
-      owner.address,
-      account1.address,
-      1,
-      1,
-      "0x00"
+    await accessToken.connect(account1).purchase(1, { value: 1 });
+
+    expect(await accessToken.authenticate(owner.address, "foo")).to.equal(
+      false
     );
-
     expect(await accessToken.authenticate(account1.address, "foo")).to.equal(
       true
     );
